87d881849de9a432f344e6932cd171207499462e
Matthew Knight <mgk1795@mail.com>
2019-09-12 22:45:02 -0700

    added to multiaddr

--- include/multiformats/multiaddr.hpp
+++ include/multiformats/multiaddr.hpp
@@ -6,7 +6,24 @@
 
 #pragma once
 
+#include "varint.hpp"
+
+#include <string>
+#include <vector>
+
+#include <cstdint>
+
 namespace Multiformats {
     class Multiaddr {
+        struct Protocol {
+            Varint code;
+            std::vector<std::uint8_t> value;
+        };
+
+        std::vector<Protocol> data;
+
+      public:
+        Multiaddr(std::string const& address);
+        std::string to_string();
     };
-}
+} // namespace Multiformats
--- include/multiformats/varint.hpp
+++ include/multiformats/varint.hpp
@@ -23,15 +23,12 @@ class Varint {
     template <typename Integral,
               typename = std::enable_if_t<std::is_integral_v<Integral>>>
     Varint(Integral integral) {
-        if constexpr (std::numeric_limits<Integral>::digits > max_bits) {
-            if (integral > max || integral < 0) {
+        if constexpr (std::numeric_limits<Integral>::digits > max_bits)
+            if (integral > max || integral < 0)
                 throw std::runtime_error("invalid value");
-            }
-        }
 
         do {
-            std::uint8_t value = integral & 0xef;
-            buf.emplace_back(value);
+            buf.emplace_back(integral & 0xef);
             integral = integral >> 7;
         } while (integral);
 
