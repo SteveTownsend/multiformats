d639c9b996509e5a04a63f54219f37df6e2e7213
Matthew Knight <mgk1795@mail.com>
2019-09-17 11:31:28 -0700

    upped gtest version

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -6,15 +6,14 @@ from conans import ConanFile, CMake, tools
 class MultiformatsTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
-    requires = "gtest/1.8.0@bincrafters/stable", "multiformats/0.1@matt1795/testing"
+    requires = "gtest/1.8.1@bincrafters/stable", "multiformats/0.1@matt1795/testing"
     def configure(self):
         self.options["gtest"].build_gmock = False
 
     def build(self):
         cmake = CMake(self)
         cmake.verbose = True
-        cmake.definitions["WITH_GMOCK"] = self.options["gtest"].build_gmock
-        cmake.configure()
+        cmake.configure(source_folder=self.source_folder)
         cmake.build()
 
     def test(self):
