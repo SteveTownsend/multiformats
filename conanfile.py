226d5aea23f225fc15c7440d2a62af8e6fc3add1
Matthew Knight <mgk1795@mail.com>
2019-09-18 11:55:58 -0700

    forcing shared for now

--- conanfile.py
+++ conanfile.py
@@ -11,18 +11,11 @@ class MultiformatsConan(ConanFile):
     homepage = "https://multiformats.io/"
     topics = ("<Put some tag here>", "<here>", "<and here>")
     settings = "os", "compiler", "build_type", "arch"
-    options = {
-        "shared": [True, False]
-    }
-    default_options = {
-        "shared": True
-    }
     generators = "cmake"
     exports_sources = "*"
 
     def build(self):
         cmake = CMake(self)
-        cmake.definitions["BUILD_SHARED_LIBS"] = "ON" if self.options.shared else "OFF"
         cmake.configure(source_folder=self.source_folder)
         cmake.build()
 
